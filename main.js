d7daf4d09d2aa2d80a408481f3a24dccee20b423
baalintka <ballint3@gmail.com>
2024-02-28 23:33:15 +0100

    rekurzio

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import { udvozles,elagazas,ciklus1,ciklus2 } from "./feladatok.js";
 import {listak,listak2} from "./listak.js";
+import {rekurzio} from "./teszt.js";
 udvozles();
 elagazas();
 ciklus1();
 ciklus2();
 listak();
 listak2();
+const tomb=[4,6,8]
+console.log(rekurzio(tomb));
---
+++ teszt.js
@@ -0,0 +1,11 @@
+let sum=0;
+export function rekurzio(arr, sum=0){
+    if (arr.length===0) return sum;
+
+    let [head, ...tail]=arr;
+
+    return rekurzio(tail,sum+head)
+        
+    
+
+}
