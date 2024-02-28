5cbebcef93b4c712a33bb28b04f3f9e25c408871
baalintka <ballint3@gmail.com>
2024-02-28 22:12:35 +0100

    s

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import { udvozles,elagazas,ciklus1,ciklus2 } from "./feladatok.js";
 import {listak,listak2} from "./listak.js";
-=======
-import { udvozles,elagazas,ciklus1,ciklus2, tomb } from "./feladatok.js";
->>>>>>> 06b5dbf8a4ab29305ca65948b15cae4991b73224
 udvozles();
 elagazas();
 ciklus1();
 ciklus2();
-<<<<<<< HEAD
 listak();
 listak2();
-=======
-tomb();
->>>>>>> 06b5dbf8a4ab29305ca65948b15cae4991b73224
